44a0aa4d86eca2878caff1c8f4b7984ddbb76241
ClaireL21 <53claire@gmail.com>
2024-08-28 10:32:46 -0400

    created bullet prefab, fires bullets

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,17 +4,72 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    // some public variables - tune ship's movement
+    // Rotate ship left & right; apply thrust in direction ship is facing
+    // results in gradual acceleration of ship
+    public Vector3 forceVector;
+    public float rotationSpeed;
+    public float rotation;
+    public GameObject bullet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        forceVector.x = 1.0f;
+        rotationSpeed = 2.0f;
+    }
+
+    /* Fixed Update performs forced changes to rigid body physics parameters 
+        - use when applying user forces / changes to rigid body components
+        - runs a fixed number of times every second (multiple times each frame)
+          rather than once per frame (Update)
+        - for forces that are continuously being applied
+     */
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(forceVector);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rotation += rotationSpeed;
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            GetComponent<Rigidbody>().MoveRotation(rot);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rotation -= rotationSpeed;
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            GetComponent<Rigidbody>().MoveRotation(rot);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 updatedPosition = gameObject.transform.position;
-        updatedPosition.x += .001f;
-        gameObject.transform.position = updatedPosition;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire! " + rotation);
+
+            /* we don’t want to spawn a Bullet inside our ship, so some
+                Simple trigonometry is done here to spawn the bullet
+                at the tip of where the ship is pointed.
+            */
+            Vector3 spawnPos = gameObject.transform.position;
+            spawnPos.x += 1.5f * Mathf.Cos(rotation * Mathf.PI / 180);
+            spawnPos.z -= 1.5f * Mathf.Sin(rotation * Mathf.PI / 180);
+
+            // instantiate the Bullet
+            GameObject obj = Instantiate(bullet, spawnPos, Quaternion.identity) as GameObject;
+
+            // get the Bullet Script Component of the new Bullet instance
+            Bullet b = obj.GetComponent<Bullet>();
+
+            // set the direction the Bullet will travel in
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            b.heading = rot;
+        }
+
     }
 }
